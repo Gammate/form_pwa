824e9c6d0c5792169faa4c566df90a906c629dcf
easa <eisanodehi@gmail.com>
2020-02-16 02:41:03 +0000

    Deploying to gh-pages from  - 51202a4da65555910fd669284e4a74ce1b5513e7 🚀

---
+++ precache-manifest.b3b48f613aa63ce88208aeed94f964bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a5300f567c5c639070bb21ba47a262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b88a0e8aa0c804b6bd93",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b88a0e8aa0c804b6bd93",
+    "url": "/static/js/2.91f9dec3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.91f9dec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec25c78888d8cf77c47",
+    "url": "/static/js/main.b62a4fcd.chunk.js"
+  },
+  {
+    "revision": "f438eef039aa462dce5c",
+    "url": "/static/js/runtime-main.cd02e29c.js"
+  }
+]);
