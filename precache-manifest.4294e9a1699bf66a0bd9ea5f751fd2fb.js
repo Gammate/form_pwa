2e23df5478978edd8411cbb0173cad608adc48df
easa <eisanodehi@gmail.com>
2020-02-16 09:31:44 +0000

    Deploying to gh-pages from  - 57a76663007182318472bbe3ef861251c8b5e08b 🚀

---
+++ precache-manifest.4294e9a1699bf66a0bd9ea5f751fd2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c8a17071c419b88d36ce718b914d46",
+    "url": "/form_pwa/index.html"
+  },
+  {
+    "revision": "b88a0e8aa0c804b6bd93",
+    "url": "/form_pwa/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b88a0e8aa0c804b6bd93",
+    "url": "/form_pwa/static/js/2.91f9dec3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/form_pwa/static/js/2.91f9dec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c71829e8faecec12ed7",
+    "url": "/form_pwa/static/js/main.15ce191a.chunk.js"
+  },
+  {
+    "revision": "bde52e786bd7c8249bbe",
+    "url": "/form_pwa/static/js/runtime-main.0deeb346.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3b48f613aa63ce88208aeed94f964bb.js"
+  "/form_pwa/precache-manifest.4294e9a1699bf66a0bd9ea5f751fd2fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/form_pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
